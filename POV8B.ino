2d8c5bd7495361861cd4f431542a623c5dbdec29
daasantos <daniloalvessantos0@gmail.com>
2017-05-10 14:41:54 -0300

    Added hexa for more letters and serial usage.
    Displayed letter will change based on data in the serial port. Inteded for usage with bluetooth. Tested with HC-05 FC-114 module on the arduino and a bluetooth adapter on a W7 with PuTTy.

--- POV8B.ino
+++ POV8B.ino
@@ -1,48 +1,91 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 8    // PIN the strip is connected to
+#define PIN 8    // Pin the strip is connected to
 #define QTLD 8   // # Neopixels in the strip
-#define DELAY 6  // Delay for each column of the image
-#define BREAK 6  // Delay for each character/image
+#define DELAY 6  // Delay for each column in a image/letter
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(QTLD, PIN, NEO_GRB + NEO_KHZ800);
+
 void setup(){
-	strip.begin();          // Initialize strip
-	strip.setBrightness(1); // Dimmer lights for POV
-	strip.show();           // Push data to strip. Since nothing has been prepared for push, all Neopixels will be set OFF
-				// show() must be called at the end of every set of changes to the bar
+	Serial.begin(9600);
+	strip.begin();          // Initialize the strip
+	strip.setBrightness(8); // Dimmer lights for POV effect
+	strip.show();           // Push data to the strip, after each call to setPixelColor(), show() must be called
 }
 
-uint8_t a[] = {0x7F,0xFF,0xC4,0xC4,0xC4,0xC4,0xFF,0x7F};
-uint8_t e[] = {0xFF,0xFF,0xDB,0xDB,0xDB,0xDB,0xDB,0xDB};
-uint8_t i[] = {0x00,0x00,0x81,0xFF,0xFF,0x81,0x00,0x00};
-uint8_t o[] = {0x7E,0xFF,0xC3,0xC3,0xC3,0xC3,0xFF,0x7E};
-uint8_t u[] = {0xFF,0xFF,0x03,0x03,0x03,0x03,0xFF,0xFF};
+uint8_t a[8] = { 0x7C, 0x7E, 0x13, 0x13, 0x7E, 0x7C, 0x00, 0x00 };
+uint8_t b[8] = { 0x41, 0x7F, 0x7F, 0x49, 0x49, 0x7F, 0x36, 0x00 };
+uint8_t c[8] = { 0x1C, 0x3E, 0x63, 0x41, 0x41, 0x63, 0x22, 0x00 };
+uint8_t d[8] = { 0x41, 0x7F, 0x7F, 0x41, 0x63, 0x3E, 0x1C, 0x00 };
+uint8_t e[8] = { 0x41, 0x7F, 0x7F, 0x49, 0x5D, 0x41, 0x63, 0x00 };
+uint8_t f[8] = { 0x41, 0x7F, 0x7F, 0x49, 0x1D, 0x01, 0x03, 0x00 };
+uint8_t g[8] = { 0x1C, 0x3E, 0x63, 0x41, 0x51, 0x73, 0x72, 0x00 };
+uint8_t h[8] = { 0x7F, 0x7F, 0x08, 0x08, 0x7F, 0x7F, 0x00, 0x00 };
+uint8_t i[8] = { 0x00, 0x41, 0x7F, 0x7F, 0x41, 0x00, 0x00, 0x00 };
+uint8_t j[8] = { 0x30, 0x70, 0x40, 0x41, 0x7F, 0x3F, 0x01, 0x00 };
+uint8_t k[8] = { 0x41, 0x7F, 0x7F, 0x08, 0x1C, 0x77, 0x63, 0x00 };
+uint8_t l[8] = { 0x41, 0x7F, 0x7F, 0x41, 0x40, 0x60, 0x70, 0x00 };
+uint8_t m[8] = { 0x7F, 0x7F, 0x0E, 0x1C, 0x0E, 0x7F, 0x7F, 0x00 };
+uint8_t n[8] = { 0x7F, 0x7F, 0x06, 0x0C, 0x18, 0x7F, 0x7F, 0x00 };
+uint8_t o[8] = { 0x1C, 0x3E, 0x63, 0x41, 0x63, 0x3E, 0x1C, 0x00 };
+uint8_t p[8] = { 0x41, 0x7F, 0x7F, 0x49, 0x09, 0x0F, 0x06, 0x00 };
+uint8_t q[8] = { 0x1E, 0x3F, 0x21, 0x71, 0x7F, 0x5E, 0x00, 0x00 };
+uint8_t r[8] = { 0x41, 0x7F, 0x7F, 0x09, 0x19, 0x7F, 0x66, 0x00 };
+uint8_t s[8] = { 0x26, 0x6F, 0x4D, 0x59, 0x73, 0x32, 0x00, 0x00 };
+uint8_t t[8] = { 0x03, 0x41, 0x7F, 0x7F, 0x41, 0x03, 0x00, 0x00 };
+uint8_t u[8] = { 0x7F, 0x7F, 0x40, 0x40, 0x7F, 0x7F, 0x00, 0x00 };
+uint8_t v[8] = { 0x1F, 0x3F, 0x60, 0x60, 0x3F, 0x1F, 0x00, 0x00 };
+uint8_t w[8] = { 0x7F, 0x7F, 0x30, 0x18, 0x30, 0x7F, 0x7F, 0x00 };
+uint8_t x[8] = { 0x43, 0x67, 0x3C, 0x18, 0x3C, 0x67, 0x43, 0x00 };
+uint8_t y[8] = { 0x07, 0x4F, 0x78, 0x78, 0x4F, 0x07, 0x00, 0x00 };
+uint8_t z[8] = { 0x47, 0x63, 0x71, 0x59, 0x4D, 0x67, 0x73, 0x00 };
 
-void displayLine(uint8_t line){
-	uint32_t color = strip.Color( 255, 0, 0 ); // R, G, B	
+void displayColumn(uint8_t col){
+	uint32_t color = strip.Color( 255, 0, 0 ); //R, G, B
+	
 	for(uint8_t i = 0; i < QTLD; i++){
-		if((0x01 << i) & line){strip.setPixelColor(i, color);} // Bitwise to choose which Neopixel(s) to set ON
+		if((0x01 << i) & col){strip.setPixelColor(i, color);} // Bitwise to select which pixels to turn on/off
 		else{strip.setPixelColor(i, 0);}
 	}
 	strip.show();
 }
 
-void displayChar(char ch){
-	if (ch == 'a'){for (uint8_t i = 0; i < 8; i++){displayLine(a[i]);delay(DELAY);}displayLine(0);}
-  	if (ch == 'e'){for (uint8_t i = 0; i < 8; i++){displayLine(e[i]);delay(DELAY);}displayLine(0);}
-  	if (ch == 'i'){for (uint8_t j = 0; j < 8; j++){displayLine(i[j]);delay(DELAY);}displayLine(0);}
-  	if (ch == 'o'){for (uint8_t i = 0; i < 8; i++){displayLine(o[i]);delay(DELAY);}displayLine(0);}
-  	if (ch == 'u'){for (uint8_t i = 0; i < 8; i++){displayLine(u[i]);delay(DELAY);}displayLine(0);}
-  	delay(BREAK);
-}
-
-void displayString(char* s){
-  	for (uint8_t i = 0; i <= strlen(s); i++){
-    		displayChar(s[i]);
-  	}
+void displayChar(char* s){
+	for (uint8_t ct = 0; ct <= strlen(s); ct++, s++){
+		if (*s == 'a'){for (uint8_t i = 0; i < 8; i++){displayColumn(a[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'b'){for (uint8_t i = 0; i < 8; i++){displayColumn(b[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'c'){for (uint8_t i = 0; i < 8; i++){displayColumn(c[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'd'){for (uint8_t i = 0; i < 8; i++){displayColumn(d[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'e'){for (uint8_t i = 0; i < 8; i++){displayColumn(e[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'f'){for (uint8_t i = 0; i < 8; i++){displayColumn(f[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'g'){for (uint8_t i = 0; i < 8; i++){displayColumn(g[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'h'){for (uint8_t i = 0; i < 8; i++){displayColumn(h[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'i'){for (uint8_t j = 0; j < 8; j++){displayColumn(i[j]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'j'){for (uint8_t i = 0; i < 8; i++){displayColumn(j[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'k'){for (uint8_t i = 0; i < 8; i++){displayColumn(k[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'l'){for (uint8_t i = 0; i < 8; i++){displayColumn(l[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'm'){for (uint8_t i = 0; i < 8; i++){displayColumn(m[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'n'){for (uint8_t i = 0; i < 8; i++){displayColumn(n[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'o'){for (uint8_t i = 0; i < 8; i++){displayColumn(o[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'p'){for (uint8_t i = 0; i < 8; i++){displayColumn(p[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'q'){for (uint8_t i = 0; i < 8; i++){displayColumn(q[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'r'){for (uint8_t i = 0; i < 8; i++){displayColumn(r[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 's'){for (uint8_t i = 0; i < 8; i++){displayColumn(s[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 't'){for (uint8_t i = 0; i < 8; i++){displayColumn(t[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'u'){for (uint8_t i = 0; i < 8; i++){displayColumn(u[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'v'){for (uint8_t i = 0; i < 8; i++){displayColumn(v[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'w'){for (uint8_t i = 0; i < 8; i++){displayColumn(w[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'x'){for (uint8_t i = 0; i < 8; i++){displayColumn(x[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'y'){for (uint8_t i = 0; i < 8; i++){displayColumn(y[i]);delay(DELAY);}displayColumn(0);}
+		if (*s == 'z'){for (uint8_t i = 0; i < 8; i++){displayColumn(z[i]);delay(DELAY);}displayColumn(0);}
+		delay(DELAY);
+	}
 }
 
 void loop(){
-  	displayString("a");
+    byte str;
+	while(Serial.available()){
+		str = Serial.read();
+		displayChar(&str);
+	}
 }
