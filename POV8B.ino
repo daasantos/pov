9e0a9bd148763e7ce9df56f9e734b1c9079949db
daasantos <daniloalvessantos0@gmail.com>
2017-06-12 23:37:53 -0300

    Changed word font, Gobetwino functions, button use

--- POV8B.ino
+++ POV8B.ino
@@ -1,48 +1,63 @@
 #include <Adafruit_NeoPixel.h>
 
-static const uint8_t pin = 8;     // Pin the strip is connected to
-static const uint8_t nLED = 8;    // # Neopixels in the strip
-static const uint8_t dTime = 8;   // Delay for each column in a image/letter
+const uint8_t pin = 8;     // Pin the strip is connected to
+const uint8_t nLED = 8;    // # Neopixels in the strip
+const uint8_t dTime = 8;   // Delay for each column in a image/letter
+const uint8_t nextLine = 9;
+const uint8_t nextSlide = 7;
+const uint8_t laserSwitch = 6;
+const uint8_t laserPin = 5;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(nLED, pin, NEO_GRB + NEO_KHZ800);
 
-String inWord, stringColor;
-uint32_t color = strip.Color(255, 0, 0);
-bool stringComplete = false;
-uint8_t a[nLED] = { 0x7C, 0x7E, 0x13, 0x13, 0x7E, 0x7C, 0x00, 0x00 };
-uint8_t b[nLED] = { 0x41, 0x7F, 0x7F, 0x49, 0x49, 0x7F, 0x36, 0x00 };
-uint8_t c[nLED] = { 0x1C, 0x3E, 0x63, 0x41, 0x41, 0x63, 0x22, 0x00 };
-uint8_t d[nLED] = { 0x41, 0x7F, 0x7F, 0x41, 0x63, 0x3E, 0x1C, 0x00 };
-uint8_t e[nLED] = { 0x41, 0x7F, 0x7F, 0x49, 0x5D, 0x41, 0x63, 0x00 };
-uint8_t f[nLED] = { 0x41, 0x7F, 0x7F, 0x49, 0x1D, 0x01, 0x03, 0x00 };
-uint8_t g[nLED] = { 0x1C, 0x3E, 0x63, 0x41, 0x51, 0x73, 0x72, 0x00 };
-uint8_t h[nLED] = { 0x7F, 0x7F, 0x08, 0x08, 0x7F, 0x7F, 0x00, 0x00 };
-uint8_t i[nLED] = { 0x00, 0x41, 0x7F, 0x7F, 0x41, 0x00, 0x00, 0x00 };
-uint8_t j[nLED] = { 0x30, 0x70, 0x40, 0x41, 0x7F, 0x3F, 0x01, 0x00 };
-uint8_t k[nLED] = { 0x41, 0x7F, 0x7F, 0x08, 0x1C, 0x77, 0x63, 0x00 };
-uint8_t l[nLED] = { 0x41, 0x7F, 0x7F, 0x41, 0x40, 0x60, 0x70, 0x00 };
-uint8_t m[nLED] = { 0x7F, 0x7F, 0x0E, 0x1C, 0x0E, 0x7F, 0x7F, 0x00 };
-uint8_t n[nLED] = { 0x7F, 0x7F, 0x06, 0x0C, 0x18, 0x7F, 0x7F, 0x00 };
-uint8_t o[nLED] = { 0x1C, 0x3E, 0x63, 0x41, 0x63, 0x3E, 0x1C, 0x00 };
-uint8_t p[nLED] = { 0x41, 0x7F, 0x7F, 0x49, 0x09, 0x0F, 0x06, 0x00 };
-uint8_t q[nLED] = { 0x1E, 0x3F, 0x21, 0x71, 0x7F, 0x5E, 0x00, 0x00 };
-uint8_t r[nLED] = { 0x41, 0x7F, 0x7F, 0x09, 0x19, 0x7F, 0x66, 0x00 };
-uint8_t s[nLED] = { 0x26, 0x6F, 0x4D, 0x59, 0x73, 0x32, 0x00, 0x00 };
-uint8_t t[nLED] = { 0x03, 0x41, 0x7F, 0x7F, 0x41, 0x03, 0x00, 0x00 };
-uint8_t u[nLED] = { 0x7F, 0x7F, 0x40, 0x40, 0x7F, 0x7F, 0x00, 0x00 };
-uint8_t v[nLED] = { 0x1F, 0x3F, 0x60, 0x60, 0x3F, 0x1F, 0x00, 0x00 };
-uint8_t w[nLED] = { 0x7F, 0x7F, 0x30, 0x18, 0x30, 0x7F, 0x7F, 0x00 };
-uint8_t x[nLED] = { 0x43, 0x67, 0x3C, 0x18, 0x3C, 0x67, 0x43, 0x00 };
-uint8_t y[nLED] = { 0x07, 0x4F, 0x78, 0x78, 0x4F, 0x07, 0x00, 0x00 };
-uint8_t z[nLED] = { 0x47, 0x63, 0x71, 0x59, 0x4D, 0x67, 0x73, 0x00 };
+String inWord;
+uint32_t color = strip.Color(255, 0, 0); // Default color
+uint8_t pid, lineMax;
+const uint8_t a[nLED] = { 0xfc,0xfe,0x13,0x13,0x13,0x13,0xfe,0xfc };
+const uint8_t b[nLED] = { 0x41,0x7f,0x7f,0x49,0x49,0x49,0x7f,0x36 };
+const uint8_t c[nLED] = { 0x3c,0x7e,0xc3,0x81,0x81,0x81,0xc3,0x42 };
+const uint8_t d[nLED] = { 0xff,0xff,0x81,0x81,0xc3,0x7e,0x3c,0x00 };
+const uint8_t e[nLED] = { 0xff,0xff,0x89,0x89,0x89,0x89,0x81,0x00 };
+const uint8_t f[nLED] = { 0xff,0xff,0x09,0x09,0x09,0x09,0x01,0x00 };
+const uint8_t g[nLED] = { 0x7e,0xff,0xc3,0x81,0x81,0xa1,0xf3,0xf2 };
+const uint8_t h[nLED] = { 0xff,0xff,0x10,0x10,0x10,0x10,0xff,0xff };
+const uint8_t i[nLED] = { 0x00,0x81,0x81,0xff,0xff,0x81,0x81,0x00 };
+const uint8_t j[nLED] = { 0x60,0xe0,0x80,0x81,0x81,0xff,0x7f,0x01 };
+const uint8_t k[nLED] = { 0xff,0xff,0x18,0x3c,0xe7,0xc3,0x00,0x00 };
+const uint8_t l[nLED] = { 0xff,0xff,0x80,0x80,0x80,0x80,0xc0,0xe0 };
+const uint8_t m[nLED] = { 0xff,0xff,0x0e,0x1c,0x1c,0x0e,0xff,0xff };
+const uint8_t n[nLED] = { 0xff,0xff,0x06,0x0c,0x18,0x30,0xff,0xff };
+const uint8_t o[nLED] = { 0x3c,0x7e,0xc3,0x81,0x81,0xc3,0x7e,0x3c };
+const uint8_t p[nLED] = { 0xff,0xff,0x09,0x09,0x09,0x0f,0x06,0x00 };
+const uint8_t q[nLED] = { 0x3e,0x7f,0x41,0x41,0xff,0xbe,0x80,0x00 };
+const uint8_t r[nLED] = { 0xff,0xff,0x09,0x09,0x19,0xff,0xe6,0x00 };
+const uint8_t s[nLED] = { 0x46,0xcf,0x8f,0x99,0x91,0xf1,0xf3,0x62 };
+const uint8_t t[nLED] = { 0x03,0x01,0x01,0xff,0xff,0x01,0x01,0x03 };
+const uint8_t u[nLED] = { 0xff,0xff,0xc0,0xc0,0xc0,0xc0,0xff,0xff };
+const uint8_t v[nLED] = { 0x3f,0x7f,0xc0,0xc0,0xc0,0xc0,0x7f,0x3f };
+const uint8_t w[nLED] = { 0xff,0xff,0x60,0x30,0x30,0x60,0xff,0xff };
+const uint8_t x[nLED] = { 0xc3,0x66,0x3c,0x18,0x18,0x3c,0x66,0xc3 };
+const uint8_t y[nLED] = { 0x0f,0x0f,0x08,0xf8,0xf8,0x08,0x0f,0x0f };
+const uint8_t z[nLED] = { 0x07,0xc3,0xe1,0xb1,0x99,0x8d,0xc7,0xe3 };
 
 void setup(){
-	Serial.begin(9600);
-	strip.begin();          	// Initialize the strip
-	strip.setBrightness(4); // Dimmer lights for POV effect
-	strip.show();           	// Push data to the strip, after each call to setPixelColor(), show() must be called
-	inWord.reserve(64);
-	stringColor.reserve(64);
+  pinMode(nextLine, INPUT_PULLUP);
+  pinMode(nextSlide, INPUT_PULLUP);
+  pinMode(laserSwitch, INPUT_PULLUP);
+  pinMode(laserPin, OUTPUT);
+  Serial.begin(9600);
+  strip.begin();          	    // Initialize the strip
+  strip.setBrightness(4);		    // Dimmer lights for POV effect
+  strip.show();           	    // Push data to the strip, after each call to setPixelColor(), show() must be called
+
+  delay(15000);
+  Serial.println("#S|RNTPD|[1]#"); // Read line number from Notepad
+  while(!Serial.available());
+  lineMax = Serial.parseInt() + 1;
+  Serial.println("#S|CHIMG|[]#"); // Start slideshow
+  while(!Serial.available());
+  pid = Serial.parseInt();
+    
 }
 
 void displayColumn(uint8_t col){
@@ -54,7 +69,8 @@ void displayColumn(uint8_t col){
 }
 
 void displayChar(String str){
-	for (uint8_t ct = 0; str.charAt(ct) != '.' ; ct++){
+	const uint8_t ct = 0;
+    
 		if (str.charAt(ct) == 'a'){ for (uint8_t i = 0; i < nLED; i++){ displayColumn(a[i]); delay(dTime); } displayColumn(0); }
 		else if (str.charAt(ct) == 'b'){ for (uint8_t i = 0; i < nLED; i++){ displayColumn(b[i]); delay(dTime); } displayColumn(0); }
 		else if (str.charAt(ct) == 'c'){ for (uint8_t i = 0; i < nLED; i++){ displayColumn(c[i]); delay(dTime); } displayColumn(0); }
@@ -82,44 +98,74 @@ void displayChar(String str){
 		else if (str.charAt(ct) == 'y'){ for (uint8_t i = 0; i < nLED; i++){ displayColumn(y[i]); delay(dTime); } displayColumn(0); }
 		else if (str.charAt(ct) == 'z'){ for (uint8_t i = 0; i < nLED; i++){ displayColumn(z[i]); delay(dTime); } displayColumn(0); }
 		delay(dTime);
-	}
+   
 }
 
-void readData(){
-  static bool readColor = false; // Needs to remember if ',' has been informed
-  while(Serial.available()){
-    char inChar = (char)Serial.read();
-    if (inChar == '.') {
-      inWord += inChar;
-      stringComplete = true;
-      readColor = false;
-      Serial.println("inWord: " + inWord);
-    }
-    else if (inChar == ','){ readColor = true; stringColor = ""; }
-  	else if (readColor){ stringColor += inChar;	Serial.println("stringColor: " + stringColor);
-  	}
-  	else { inWord += inChar; Serial.println("INWORD OK"); }
-  }
+bool readData(){
+  bool readColor = false;
+  String stringColor;
+  static uint8_t line = 2;
+  static bool laserStatus = false;
   
-  if (stringColor == "vermelho") color = strip.Color(255, 0, 0);
-  else if (stringColor == "verde") color = strip.Color(0, 255, 0);
-  else if (stringColor == "azul") color = strip.Color(0, 0, 255);
-  else if (stringColor == "amarelo") color = strip.Color(255, 255, 0);
-  else if (stringColor == "roxo") color = strip.Color(127, 0, 255);
-  else if (stringColor == "laranja") color = strip.Color(255, 128, 0);
-  else if (stringColor == "cinza") color = strip.Color(128, 128, 128);
+  if(digitalRead(nextLine) == LOW) { 
+    line++; 
+    delay(200); 
+  }
+  if(digitalRead(laserSwitch) == LOW) {
+    if(!laserStatus) digitalWrite(laserPin, HIGH);
+    else digitalWrite laserPin, LOW);
+    laserStatus = !laserStatus;
+    delay(200);
+  }
+  if(digitalRead(nextSlide) == LOW) {
+    Serial.print("#S|SENDK|[");
+    Serial.print(pid);
+    Serial.println("& {RIGHT} ]#");
+    delay(200);
+  }
+  if(line >= lineMax) line = 2;
+    
+  Serial.print("#S|RNTPD|["); // Read line from Notepad
+  Serial.print(line);
+  Serial.println("]#");
+    
+  while(Serial.available()){
+    
+    char inChar = (char)Serial.read();
+  
+    if (inChar == '.') {
+      if (readColor){
+          if (stringColor == "vermelho") color = strip.Color(255, 0, 0); // red
+          else if (stringColor == "verde") color = strip.Color(0, 255, 0); // green
+          else if (stringColor == "azul") color = strip.Color(0, 0, 255); // blue
+          else if (stringColor == "roxo") color = strip.Color(128, 0, 255); // purple
+          else if (stringColor == "rosa") color = strip.Color(255, 0, 255); // pink
+          else if (stringColor == "turquesa") color = strip.Color(0, 255, 255); // teal
+          else if (stringColor == "branco") color = strip.Color(255, 255, 255); // white
+      }
+    Serial.println("stringColor: " + stringColor);
+    readColor = false;
+	  return true;
+    }
+    else if (readColor) stringColor += inChar;
+    else if (inChar == ','){
+      readColor = true;
+      stringColor = "";
+      inWord += '.';
+  }
+    else inWord += inChar;
+	
+  }
+  return false;
 }
 
 void loop(){
-  static String str;		   // Keeps last used string
-  static bool started = false; // Remembers if a word has been informed since the program began
-  readData();
-  if (stringComplete){
-    Serial.println("STRING COMPLETE");
+  static String str = "a"; // Default
+  
+  if (readData()){
+    Serial.println("inWord: " + inWord);
     str = inWord;
     inWord = "";
-    started = true;
-    stringComplete = false;
   }
-  if (started) displayChar(str);
+  displayChar(str);
 }
